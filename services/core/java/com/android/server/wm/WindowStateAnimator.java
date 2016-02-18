9facac75fdd1b69c8a9b0314b8f52594b322b28d
ChangzhiX Wei <changzhix.wei@intel.com>
2016-02-18 15:19:31 +0800

    Only window position changed, update window crop.
    When the window move to the edge of screen, first call setWindowCrop,
    if the position of window is changed, second call setPosition.
    setWindowCrop will let surfaceflinger dispaly the window, but the
    lefttop position is the last postion, this will lead the rightbottom
    position reduced, and the reduced rect is dispaly the second layer's
    window, so the window flicking.
    setPosition let surfaceflinger dispaly the window second, and this
    time is right rect.
    
    Solution:  When the window move to the edge of screen, only the
    position of window is changed, call setPosition and setWindowCrop.
    
    Change-Id: I235253a02cf7437cfeb9971231ee559d3b8706b3
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- services/core/java/com/android/server/wm/WindowStateAnimator.java
+++ services/core/java/com/android/server/wm/WindowStateAnimator.java
@@ -1419,6 +1419,7 @@ class WindowStateAnimator {
                 if (WindowManagerService.SHOW_TRANSACTIONS) WindowManagerService.logSurface(w,
                         "POS " + left + ", " + top, null);
                 mSurfaceControl.setPosition(left, top);
+                updateSurfaceWindowCrop(recoveringMemory);
             } catch (RuntimeException e) {
                 Slog.w(TAG, "Error positioning surface of " + w
                         + " pos=(" + left + "," + top + ")", e);
@@ -1441,6 +1442,7 @@ class WindowStateAnimator {
                 mSurfaceControl.setMatrix(
                         mDsDx * w.mHScale, mDtDx * w.mVScale,
                         mDsDy * w.mHScale, mDtDy * w.mVScale);
+                updateSurfaceWindowCrop(recoveringMemory);
                 mAnimator.setPendingLayoutChanges(w.getDisplayId(),
                         WindowManagerPolicy.FINISH_LAYOUT_REDO_WALLPAPER);
                 if ((w.mAttrs.flags & LayoutParams.FLAG_DIM_BEHIND) != 0) {
@@ -1461,7 +1463,7 @@ class WindowStateAnimator {
             }
         }
 
-        updateSurfaceWindowCrop(recoveringMemory);
+        //updateSurfaceWindowCrop(recoveringMemory);
     }
 
     public void prepareSurfaceLocked(final boolean recoveringMemory) {
